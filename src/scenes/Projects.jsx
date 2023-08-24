efcec97361203deb1acfab5b05573a26b8c6e196
Kungfu90 <andrea.siano@studio.unibo.it>
2023-08-24 16:11:52 +0200

    Fixing opacity

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -1,5 +1,5 @@
 import img1 from "./../assets/images/travel-app.png";
-import img2 from "./../assets/images/east-asian-blog.png";
+import img2 from "./../assets/images/Blog.png";
 import { motion } from "framer-motion";
 
 export default function Projects() {
@@ -28,7 +28,7 @@ export default function Projects() {
               <h1 class="text-white max-w-[500px] xxs:text-2xl sm:text-3xl md:text-4xl leading-10 font-teko lg:text-5xl mb-2 font-bold">
                 TRAVEL MOBILE APP ADVENTURE-VIBES
               </h1>
-              <p className="font-sans text-white pb-5 font-regular max-w-[400px] xxs:text-[15px] text-[20px] ">
+              <p className="font-sans text-white pb-5 font-regular max-w-[400px] xxs:text-[15px] lg:text-[20px] ">
                 This is a complete travel mobile app experience built with
                 figma. You can take a look at it if you want to!
               </p>
@@ -44,6 +44,7 @@ export default function Projects() {
               class="md:h-[50vh] lg:h-[80vh] sm:h-[70vh] xxs:h-[60vh] w-full object-cover rounded-md"
               src={img2}
             ></img>
+            <div class="absolute inset-0 bg-black opacity-30 rounded-md"></div>
             <div class="absolute inset-0 flex items-start flex-col justify-center px-6">
               <h4 className="font-teko text-gray-400 pb-5 font-regular max-w-[400px] text-[20px] ">
                 FRONT END DEVELOPMENT
@@ -58,7 +59,7 @@ export default function Projects() {
               </p>
               <button class="lg:p-6 xxs:p-2 xxs:text-xl lg:text-2xl font-teko bg-transparent hover:bg-white text-white font-regular hover:text-black transition duration-500 py-2 px-4 border border-white hover:border-transparent rounded">
                 <a href="https://east-asian-blog.netlify.app/">
-                  SEE MY PROTOTYPE
+                  SEE MY PROJECT
                 </a>
               </button>
             </div>
