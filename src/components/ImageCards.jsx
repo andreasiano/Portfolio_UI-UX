ce41b022149548afd156c577c1d1059423935f1f
Kungfu90 <andrea.siano@studio.unibo.it>
2023-09-20 21:04:46 +0200

    Adding Card Images

--- src/components/ImageCards.jsx
+++ src/components/ImageCards.jsx
@@ -14,16 +14,18 @@ export default function ImageCards() {
             src={item.img}
             lt={item.title}
           />
-          <div className="absolute p-4 text-slate-900 inset-0 flex flex-col items-start bg-gradient-to-t from-black/40 to transparent">
-            <h1 className="text-[50px] font-bold">{item.title}</h1>
-            <p className="max-w-[500px] text-xl mb-5">{item.description}</p>
-            <button className="bg-white hover:bg-slate-100 text-slate-900 font-bold py-2 px-4 rounded">
-              <a
-                className="text-xl"
-                href="https://east-asian-blog.netlify.app/"
-              >
-                {item.button}
-              </a>
+          <div className="absolute lg:mt-7 sm:mt-5 lg:mx-4 p-4 text-slate-900 inset-0 flex flex-col items-start">
+            <h1 className="lg:text-[50px] sm:text-[50px] xxs:text-xl xxs:mb-4 font-bold">
+              {item.title}
+            </h1>
+            <p className="max-w-[500px] lg:text-xl sm:text-2xl sm:mb-5 sm:mt-5 lg:mb-5 xxs:mb-2">
+              {item.description}
+            </p>
+            <button
+              type="button"
+              className="inline-flex sm:text-2xl items-center lg:text-xl">
+              {item.button}
+              <a href='' className='ml-3'>{item.icon}</a>
             </button>
           </div>
         </div>
