1c8ea5fbe99299c6c1ccbf9368e8f1c8d1977169
Kungfu90 <andrea.siano@studio.unibo.it>
2023-09-20 08:36:18 +0200

    Reducing h1 size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,12 +11,12 @@ const Hero = () => {
       <div
         className={`flex-2 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row justify-between items-center w-full">
-          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[42px] text-white ss:leading-[100.8px] leading-[55px]">
             Building <br className="sm:block hidden" />{" "}
             <span className="text-gradient">Digital Products</span>,{" "}
           </h1>
         </div>
-        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[42px] text-white ss:leading-[100.8px] leading-[55px] w-full">
           Coding and More...
         </h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
