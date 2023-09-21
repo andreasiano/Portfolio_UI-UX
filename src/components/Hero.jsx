d0ac0380c72f362a8dd800a854b47749130ff1e5
Kungfu90 <andrea.siano@studio.unibo.it>
2023-09-21 19:18:59 +0200

    Fixing Image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className={`flex md:flex-row flex-col ${styles.paddingY}`}
+      className={`flex md:flex-row gap-10 flex-col ${styles.paddingY}`}
     >
       <div
         className={`flex-2 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
