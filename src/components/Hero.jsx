a0148c5131b018c29c308487c85d730377fd2889
Andrea S. Pensieri <107196739+andreasiano@users.noreply.github.com>
2023-12-05 19:01:03 +0100

    Add files via upload

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -33,9 +33,7 @@ const Hero = () => {
         <img
           src={portrait}
           alt="portrait"
-          className="w-[100%] h-[100%] rounded-xl xxs:mb-30 relative z-[5]"
-          width='640px'
-          height='720px'
+          className="w-full h-full rounded-xl xxs:mb-30 relative z-[5]"
         />
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
